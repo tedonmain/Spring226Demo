b539dd1f8217d550b8b69e75cf5bd304516fb8cf
tedonmain <tedonmain@gmail.com>
2020-04-10 10:05:40 -0700

    JUnit testing

--- src/main/java/com/demo/GreeterBase.java
+++
@@ -1,6 +0,0 @@
-package com.demo;
-
-public abstract class GreeterBase implements GreetInterface {
-
-    protected int greetCount = 0;
-}
--- src/main/java/com/demo/RestController.java
+++ src/main/java/com/demo/RestController.java
@@ -1,34 +1,11 @@
 package com.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @org.springframework.web.bind.annotation.RestController
 public class RestController {
 
     @Autowired
-    private Greeter greeter;
-
-    @Autowired
-    private FriendlyGreeter friendlyGreeter;
-
-    @Autowired
-    private GreetInterface greetAgain;
-
-    @Autowired(required = false)
-    private NotActuallyABean notABean;
-
-    @RequestMapping("/hello")
-    public String hello() {
-        return greeter.greet();
-    }
-
-    @RequestMapping("/hellofriendly")
-    public String helloFriendly() {
-        return friendlyGreeter.greet();
-    }
-
-    @RequestMapping("/helloagain")
-    public String helloAgain() { return greetAgain.greet(); }
+    private CustomerRepository repository;
 
 }
