62b944215abad1c767ccd9b8adf70960629c103a
tedonmain <tedonmain@gmail.com>
2020-04-11 13:33:37 -0700

    Add rest endpoints get, post, put, delete

--- src/main/java/com/demo/CustomerController.java
+++ src/main/java/com/demo/CustomerController.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
+package com.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
 public class CustomerController {
+
+    @Autowired
+    private CustomerRepository repository;
+
+    @RequestMapping(value = "/customer", method = RequestMethod.POST)
+    public Customer save(@RequestBody Customer customer){
+        return repository.save(customer);
+    }
+
+    @RequestMapping(value = "/customer", method = RequestMethod.PUT)
+    public Customer update(@RequestBody Customer customer){
+        return repository.save(customer);
+    }
+
+    @RequestMapping(value = "/customer/{id}", method = RequestMethod.GET)
+    public Customer get(@PathVariable Long id){
+        return repository.findById(id).orElse(null);
+    }
+
+    @RequestMapping(value = "/customer/{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable Long id){
+        repository.deleteById(id);
+    }
+
 }
